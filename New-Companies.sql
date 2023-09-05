146cdcd15fbd21aee441b3bc2018a5cd83bc3bcf
unknown <erpriyankasawant@gmail.com>
2023-09-05 11:41:13 -0400

    New-Companies

---
+++ New-Companies.sql
@@ -0,0 +1,15 @@
+Ambers conglomerate corporation just acquired some new companies. Each of the companies follows this hierarchy: 
+Given the table schemas below, write a query to print the company_code, founder name, total number of lead managers, total number of senior managers, total number of managers, and total number of employees. Order your output by ascending company_code.
+Note:
+The tables may contain duplicate records.
+The company_code is string, so the sorting should not be numeric. For example, if the company_codes are C_1, C_2, and C_10, then the ascending company_codes will be C_1, C_10, and C_2.
+
+SELECT e.company_code, c.founder, 
+       COUNT(DISTINCT e.lead_manager_code), 
+       COUNT(DISTINCT e.senior_manager_code), 
+       COUNT(DISTINCT e.manager_code),
+       COUNT(DISTINCT e.employee_code)
+FROM Company c
+INNER JOIN Employee e ON c.company_code = e.company_code
+GROUP BY e.company_code, c.founder
+ORDER BY e.company_code ASC
